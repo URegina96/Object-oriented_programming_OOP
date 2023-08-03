329467eac220a2db38a2e565501cb7ca61f4841d
Regina <126401949+URegina96@users.noreply.github.com>
2023-08-03 17:54:02 +0500

    Constructors
    Прошла/повторила  и добавила наглядность конструкторов

--- OOP/src/Box.java
+++ OOP/src/Box.java
@@ -2,7 +2,16 @@ public class Box {
     double length;
     double width;
     double height;
-
+    Box() { //конструктор
+        this.length = 10;
+        this.width = 10;
+        this.height = 10;
+    }
+    Box(double length, double width, double height) { //конструктор
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
     void setDimens(double length, double width, double height) { //метод устанавливающий значения
         this.length = length;
         this.width = width;
--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Box box1 = new Box();
-        box1.setDimens(10, 10, 10);
-        Box box2 = new Box();
-        box1.setDimens(20, 20, 20);
+        //box1.setDimens(10, 10, 10);
+        Box box2 = new Box(20, 20, 20);
+        //box1.setDimens(20, 20, 20);
         box1.showVolume();
         box2.showVolume();
 //        Test test = new Test(); //экземпляр класса
